1b0ded1dc6b5efb6d0f802666dd9fe383a83b5d9
jesse.zha <jesse.zha@swwcc.com.au>
2018-02-09 23:56:13 +1100

    search page added

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -14,12 +14,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'search.html',
 })
 export class SearchPage {
+  public search_list = ['Clothing','Shoes','Electronics','E-Readers'];
+  public flag : boolean; // if true then show search result
+  public list = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    for(let i=0;i<10;i++){
+      this.list.push({
+        pic: 'assets/imgs/0' +i+ '.jpg',
+        title: i+'th'
+      });
+    } 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
+  }
+
+  getItems(event: any){
+    let val = event.target.value;
+    
+    // if the value is empty string then don't show search result
+    if(val && val.trim() != '') {
+      this.flag = true;
+
+    } else {
+      this.flag = false;
+    }
+
+    
+  }
+  onItem(){
+    console.log('Item clicked');
   }
 
 }
